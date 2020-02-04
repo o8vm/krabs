7becf98e82045f050b41c6c82a957c04699f2284
ellbrid <ell@exoskeleton.dev>
2020-02-05 00:29:59 +0900

    Fixed bz2 and memory layout.

--- src/stage_4th/src/bz2d.rs
+++ src/stage_4th/src/bz2d.rs
@@ -14,18 +14,30 @@ extern "C" {
     ) -> i32;
 }
 
-pub fn decompress_kernel(
-    dest: *const u8,
-    dest_len: *const u32,
-    source: *const u8,
-    source_len: u32,
-    small: i32,
-    verbosity: i32,
-) -> Result<(), &'static str> {
-    let ret =
-        unsafe { BZ2_bzBuffToBuffDecompress(dest, dest_len, source, source_len, small, verbosity) };
+pub fn decompress_kernel(source_len: u32) -> Result<(), &'static str> {
+    use plankton::{ELF_START, IMAGE_START};
+    let buf_size = IMAGE_START - ELF_START;
+    let ret = unsafe {
+        BZ2_bzBuffToBuffDecompress(
+            ELF_START as *const u8,
+            &buf_size as *const u32,
+            IMAGE_START as *const u8,
+            source_len,
+            0,
+            0,
+        )
+    };
     match ret {
         0 => Ok(()),
+        -1 => Err("Invaild data structures (buffers etc)"),
+        -2 => {
+            Err("A parameter to a function call is out of range or otherwise manifestly incorrect")
+        }
+        -3 => Err("A request to allocate memory failed"),
+        -4 => Err("A data integrity error is detected"),
+        -5 => Err("The compressed stream does not start with the correct magic bytes"),
+        -7 => Err("The compressed file finishes before the logical end of stream is detected"),
+        -8 => Err("The output data will not fit into the output buffer provided"),
         _ => Err("failed to decompress the image"),
     }
 }
--- src/stage_4th/src/main.rs
+++ src/stage_4th/src/main.rs
@@ -1,27 +1,18 @@
 #![no_std]
 #![no_main]
 
-use plankton::{ELF_START, IMAGE_START, INIT_SEG, KERNEL_SIZE};
-use stage_4th::bz2d::decompress_kernel;
-use stage_4th::svm;
-use stage_4th::{clear_bss, loader::load_elf, loader::GuestAddress, print, println};
+use plankton::{INIT_SEG, KERNEL_SIZE};
+use stage_4th::{
+    bz2d::decompress_kernel, clear_bss, loader::load_elf, loader::GuestAddress, print, println, svm,
+};
 
 #[link_section = ".first"]
 #[no_mangle]
 fn stage4() -> ! {
     clear_bss();
-    let buf_size = IMAGE_START - ELF_START;
     let kernel_size = unsafe { *((KERNEL_SIZE + (INIT_SEG << 4)) as *const u32) };
     println!("  Decompressing kernel ...");
-    decompress_kernel(
-        ELF_START as *const u8,
-        &buf_size as *const u32,
-        IMAGE_START as *const u8,
-        kernel_size,
-        0,
-        0,
-    )
-    .unwrap();
+    decompress_kernel(kernel_size).unwrap();
 
     print!("  Relocating ");
     let entry_addr = load_elf(kernel_size)
